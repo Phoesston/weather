2012ae6f8bf35f25a01ef978d5e982e3bfc3bf9e
Phoesston <joshua.jr2017@gmail.com>
2025-10-19 21:22:51 -0500

    Weather app 70% done

--- src/scripts/fetchWeatherAPI.js
+++ src/scripts/fetchWeatherAPI.js
@@ -1,8 +1,8 @@
-const API_KEY = "UMV8FJHJSU28EEFG8BBPZRAN9";
+export const API_KEY = "89f6596948ee4f748e9123610251910";
 
 export async function getWeather(location){   
      try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API_KEY}`);
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=yes`);
 
         if(!response.ok){
             throw new Error('Location was not found');
@@ -19,6 +19,24 @@ export async function getWeather(location){
         throw error;
      }
 
+}
+
+export async function getForecast(location) {
+   try {
+      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`);
+      if(!response.ok){
+         throw new Error('Houly location not found');
+      }
+
+      const forecastData = await response.json();
+
+      console.log('forecast data: ', forecastData);
+
+      return forecastData;
+   } catch (error) {
+      console.error('Fetching Error: ', error);
+      throw error;
+   }
 }
 
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,34 +1,112 @@
-import { getWeather, searchLocation } from './fetchWeatherAPI.js';
-import { getCitySuggestions } from './autoComplete.js';
+import { getWeather, getHourly, getForecast} from './fetchWeatherAPI.js';
+import { autoComplete } from './autoComplete.js';
+
+import '../styles/styles.css';
 
 const input = document.getElementById('cityInput');
-const suggestions = document.getElementById('suggestions');
+const suggestions = document.querySelector('.suggestions');
 const weatherInfo = document.getElementById('weatherInfo');
+const currentWeather = document.getElementById('current-weather');
+const weatherGraphics = document.querySelector('.weather-graphics');
+const weatherHourly = document.querySelector('.weather-hourly');
+const weatherWeekly = document.querySelector('.weather-weekly');
 
 let location = 'Tampa'
 
+async function renderWeather(location){
+    try{
+        const processedData = await getWeather(location);
+        const forecastData = await getForecast(location);
+
+        const current = processedData.current;
+        const condition = current.condition;
+
+        currentWeather.innerHTML = `
+            <div id="current-temperature">
+                    <h2>${processedData.location.name}, ${processedData.location.region}, ${processedData.location.country}</h2>
+                    <h1>${current.temp_f}°F</h1>
+                    <div class="description">
+                        <p>${condition.text}</p>
+                        <p>feels like ${current.feelslike_f}°F</p>
+                    </div>
+                </div>
+        `;
+
+        weatherGraphics.innerHTML = `
+            <img src="https:${condition.icon}" alt="${condition.text}">
+            <p>Humidity: ${current.humidity}%</p>
+            <p>UV Index: ${current.uv}</p>
+        `;
+
+        //hourly forecast
+        forecastData.forecast.forecastday[0].hour.forEach(hour => {
+            const hourCard = document.createElement('div');
+            hourCard.classList.add('hour-card');
+
+            hourCard.innerHTML = `
+                <p>${hour.time.split(' ')[1]}</p>
+                <img src="https:${hour.condition.icon}" alt="${hour.condition.text}">
+                <p>${hour.temp_f}°F</p>
+            `;
+            weatherHourly.appendChild(hourCard);
+        });
+
+        //weekly forcast
+        forecastData.forecast.forecastday.forEach(day =>{
+            const dayCard = document.createElement('div');
+            dayCard.classList.add('hour-card');
+
+            const date = new Date(day.date);
+            const options = { weekday: 'short', month: 'short', day: 'numeric' };
+
+            dayCard.innerHTML = `
+                <p>${date.toLocaleDateString(undefined, options)}</p>
+                <img src="https:${day.day.condition.icon}" alt="${day.day.condition.text}">
+                <p>Max: ${day.day.maxtemp_f}°F</p>
+                <p>Min: ${day.day.mintemp_f}°F</p>
+            `;
+            weatherWeekly.appendChild(dayCard);
+        });
+
+    }catch(error){
+        weatherInfo.innerHTML = `<p>Error fetching weather: ${error.message}</p>`;
+    }
+}
+
 input.addEventListener('keypress', async (e) => {
+    suggestions.innerHTML = ''
+    suggestions.classList.add('active');
+
+    //search autocorrect
+    const searchedData = await autoComplete(input.value.trim());
+    for(let i = 0; i<Object.keys(searchedData).length; i++){
+       
+        const li = document.createElement('li');
+        li.textContent = `${searchedData[i].name}, ${searchedData[i].region}, ${searchedData[i].country}`;
+
+        li.addEventListener('click', async () =>{
+            location = searchedData[i].name;
+            renderWeather(location);
+
+            suggestions.innerHTML = ''
+            suggestions.classList.remove('active');
+        });
+
+        suggestions.appendChild(li);       
+    }    
+
+    
+
+    
     if(e.key == 'Enter'){
         e.preventDefault();
         location = input.value.trim();
+
+        suggestions.innerHTML = ''
+        suggestions.classList.remove('active');
+
         if(location){
-           try{
-            const processedData = await getWeather(location);
-
-            const current = processedData.currentConditions;
-
-            weatherInfo.innerHTML = `
-                    <p>Resolved Address: ${processedData.resolvedAddress}</p>
-                    <p>Temperature: ${current.temp}°F</p>
-                    <p>humidity: ${current.humidity}</p>
-                    <p>Condition: ${current.icon}</p>
-                    <p>Uv Index: ${current.uvindex}</p>
-                    <p>description: ${processedData.description}</p>
-                 
-                `;
-           }catch(error){
-            weatherInfo.innerHTML = `<p>Error fetching weather: ${error.message}</p>`;
-           }
+           renderWeather(location);
         }
     }
     
