11915e3c72d41fc4ecb164bbe53d729d01ec4be8
Phoesston <joshua.jr2017@gmail.com>
2025-10-22 22:05:41 -0500

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath:"",
     clean: true,
   },
   devtool: "eval-source-map",
@@ -21,6 +22,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+      filename: "index.html"
     }),
   ],
   module: {
